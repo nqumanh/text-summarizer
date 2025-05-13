ada6c9ba81a402a9ed17e8a36316bc7c7fe8e618
nqumanh <nqumanh@gmail.com>
2025-05-13 13:32:07 +0700

    feat: commit for demo

--- backend/server.py
+++ backend/server.py
@@ -5,12 +5,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-model_name = "t5-base"
+model_name = "t5-small"
 tokenizer = T5Tokenizer.from_pretrained(model_name)
-model = T5ForConditionalGeneration.from_pretrained(model_name)
-# model_name = "google/pegasus-large"
-# tokenizer = PegasusTokenizer.from_pretrained(model_name)
-# model = PegasusForConditionalGeneration.from_pretrained(model_name)
+trained_model_path = "./model/"
+model = T5ForConditionalGeneration.from_pretrained(trained_model_path)
 
 @app.route('/summarize', methods=['POST'])
 def summarize_text():
@@ -23,17 +21,18 @@ def summarize_text():
     input_text = "summarize: " + text
     input_ids = tokenizer.encode(input_text, return_tensors="pt", max_length=512, truncation=True)
 
-    summary_ids = model.generate(
-        input_ids,
-        max_length=200,
-        min_length=100,
-        do_sample=True,
-        temperature=0.8,
-        top_k=50,
-        top_p=0.95,
-        num_return_sequences=1,
-        early_stopping=True
-    )
+    # summary_ids = model.generate(
+    #     input_ids,
+    #     max_length=200,
+    #     min_length=100,
+    #     do_sample=True,
+    #     temperature=0.8,
+    #     top_k=50,
+    #     top_p=0.95,
+    #     num_return_sequences=1,
+    #     early_stopping=True
+    # )
+    summary_ids = model.generate(input_ids, max_length=200, num_beams=4, early_stopping=True)
 
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return jsonify({"summary": summary})
